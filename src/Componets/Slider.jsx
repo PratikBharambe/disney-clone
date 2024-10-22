486dd97283a8df8144aa3e8b7007f5a7a0102387
SuraCdac <sura.cdac@gmail.com>
2024-10-22 06:22:58 +0530

    ui enhancement

--- src/Componets/Slider.jsx
+++ src/Componets/Slider.jsx
@@ -26,10 +26,10 @@ function Slider() {
   return (
     <div>
         <HiChevronLeft className="hidden md:block text-white text-[30px] absolute
-        mx-8 mt-[150px] cursor-pointer " 
+        mx-8 mt-[250px] cursor-pointer " 
         onClick={()=>sliderLeft(elementRef.current)}/>
         <HiChevronRight className='hidden md:block text-white text-[30px] absolute
-        mx-8 mt-[150px] cursor-pointer right-0' 
+        mx-8 mt-[250px] cursor-pointer right-0' 
         onClick={()=>sliderRight(elementRef.current)}/>
 
    
@@ -37,7 +37,7 @@ function Slider() {
     scrollbar-none scroll-smooth' ref={elementRef}> 
         {movieList.map((item)=>(
             <img src={IMAGE_BASE_URL+item.backdrop_path} 
-            className='min-w-full  md:h-[310px] object-cover
+            className='min-w-full  md:h-[490px] object-cover
             object-left-top mr-5 rounded-md hover:border-[4px]
             border-gray-400 transition-all duration-100 ease-in'/>
         ))}
